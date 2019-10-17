3251c7ece5abc494d2dd726de357ed8af1a8c232
Yisus <yisusjuarezo@hotmail.com>
2019-10-17 09:49:24 -0500

    Api - IMG & INDArt, Inicio

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.get('/api/inicio', async function (req, res) {
     // make sure that any items are correctly URL encoded in the connection string
     await sql.connect(dbConfig)
     //const result = await sql.query`select * from mytable where id = ${value}`
-    var result = await sql.query`SELECT TOP 9 Nombre, ImgUrl, FORMAT(Fecha,'yyyy-MM-dd HH:mm:ss') as Fecha FROM Articles ORDER BY IdArticle ASC`
+    var result = await sql.query`SELECT TOP 9 Nombre, ImgUrl, FORMAT(Fecha,'yyyy-MM-dd HH:mm:ss') as Fecha FROM Articles ORDER BY cast([Fecha] as datetime) DESC`
     var inicio = JSON.parse(JSON.stringify(result))
-    console.log(typeof inicio)
+
     res.json({ data: inicio });
 
 });
@@ -45,46 +45,45 @@ app.get('/api/inicio', async function (req, res) {
 app.get('/api/articulo/:art', async function (req, res) {
     // make sure that any items are correctly URL encoded in the connection string
     await sql.connect(dbConfig)
-    var ImgUrl =  req.params.art;
+    var ImgUrl = req.params.art;
     //const result = await sql.query`select * from mytable where id = ${value}`
-    var result = await sql.query`SELECT TOP 1 a.IdArticle, a.Url, a.Nombre, FORMAT(Fecha,'yyyy-MM-dd HH:mm:ss') as Fecha, a.Body, a.ImgUrl, u.UserName, s.Nombre AS Section FROM Articles a 
+    var result = await sql.query`SELECT TOP 1 a.Id, a.Url, a.Nombre, FORMAT(Fecha,'yyyy-MM-dd HH:mm:ss') as Fecha, a.Body, a.ImgUrl, u.UserName, s.Nombre AS Section FROM Articles a 
     INNER JOIN Users u ON a.IdUser = u.IdUser
     INNER JOIN Sections s ON a.IdSection = s.IdSection
     WHERE a.Url = ${ImgUrl}`
-    var result2 = await sql.query`select top 4 Url, Nombre, ImgUrl  from Articles`
+    var result2 = await sql.query`select top 4 Url, Nombre, ImgUrl  from Articles WHERE Url != ${ImgUrl}`
     var inicio = JSON.parse(JSON.stringify(result.concat(result2)))
-    console.log(result)
     res.json({ data: inicio });
-
+    console.log(inicio)
 });
 
-var executeQuery = function (res, query) {
-    sql.connect(dbConfig, function (err) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // create Request object
-            var request = new sql.Request();
-            // query to the database
-            request.query(query, function (err, recordset) {
-                if (err) {
-                    console.log("Error while querying database :- " + err);
-                    res.send(err);
-                }
-                else {
-                    res.send(recordset);
-                }
-            });
-        }
-    });
-}
+
+// var executeQuery = function (res, query) {
+//     sql.connect(dbConfig, function (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             // create Request object
+//             var request = new sql.Request();
+//             // query to the database
+//             request.query(query, function (err, recordset) {
+//                 if (err) {
+//                     console.log("Error while querying database :- " + err);
+//                     res.send(err);
+//                 }
+//                 else {
+//                     res.send(recordset);
+//                 }
+//             });
+//         }
+//     });
+// }
 //GET IMG
-app.get("/api/img/:img", function (req,res){
+app.get("/api/img/:img", function (req, res) {
     var url = req.params.img;
     var imgsrc = `./public/${url}.jpg`;
     res.sendfile(imgsrc);
-    console.log(imgsrc);
 })
 
 // //Más artículos diferentes del actual para BlogPost
